099f8189a2a1f7c3393bf8033926e08b0d062dbf
maigithome <mai7@bk.ru>
2022-09-15 13:48:53 +0500

    commit3

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿string[] array = new string[] { "hello", "2", "world", ":-)", "123", "каша" };
+string[] resArray = new string[array.Length];
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        resArray[i] = array[i];
+    }
+}
+
+
